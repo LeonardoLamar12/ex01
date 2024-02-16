fcb9cfc7b422c7c20c61756e83f240e89749a4f9
LeonardoLamar12 <160190496+LeonardoLamar12@users.noreply.github.com>
2024-02-16 20:38:11 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,16 @@
+var total = 10+5*2/2+20;
+let nome = 'Mauricio';
+var idade = 50;
+let n1 = '200';
+var incremento = 5;
+
+var numero = '80';
+var unidade = 'Kg';
+var peso = numero + unidade;
+var pesoPorDois = numero / 2+unidade;
+
+console.log(total)
+console.log(total+nome,nome+idade)
+console.log(+n1+idade)
+console.log(++incremento)
+console.log(pesoPorDois)
